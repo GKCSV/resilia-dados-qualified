7bed3c3193034fa8d81c4ec94ed2b0a91a455a88
Gabriel Kanarski <gkcsvbls@gmail.com>
2022-05-27 23:24:19 -0300

    Criação do arquivo .py
    Criação do arquivo qualified_podio.py contendo o código da 1ª questão da semana 4

---
+++ qualified_podio.py
@@ -0,0 +1,22 @@
+def podio_olimpico(tempo_chegada1, tempo_chegada2, tempo_chegada3):
+    t1 = int(tempo_chegada1)
+    t2 = int(tempo_chegada2)
+    t3 = int(tempo_chegada3)
+
+    if (t1 < t2 and t1 < t3) and (t2 < t3):
+        return print("1 -", t1, "minutos\n2 -", t2, "minutos\n3 -", t3, "minutos")
+    
+    elif (t1 < t2 and t1 < t3) and (t2 > t3):
+        return print("1 -", t1, "minutos\n2 -", t3, "minutos\n3 -", t2, "minutos")
+
+    elif (t2 < t1 and t2 < t3) and (t1 < t3):
+        return print("1 -", t2, "minutos\n2 -", t1, "minutos\n3 -", t3, "minutos")
+    
+    elif (t2 < t1 and t2 < t3) and (t1 > t3):
+        return print("1 -", t2, "minutos\n2 -", t3, "minutos\n3 -", t1, "minutos")
+    
+    elif (t3 < t1 and t3 < t2) and (t1 < t2):
+        return print("1 -", t3, "minutos\n2 -", t1, "minutos\n3 -", t2, "minutos")
+    
+    elif (t3 < t1 and t3 < t2) and (t1 > t2):
+        return print("1 -", t3, "minutos\n2 -", t2, "minutos\n3 -", t1, "minutos")
